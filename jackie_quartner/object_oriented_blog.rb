e06a82035ec42a89c244012d85d12413acae26d2
Jackie Quartner <jmquartner@gmail.com>
2014-09-29 21:34:46 -0400

    Adding object oriented blog exercise again

--- jackie_quartner/object_oriented_blog.rb
+++ jackie_quartner/object_oriented_blog.rb
@@ -62,8 +62,7 @@ class Author
   # string interpolation to join together the first_name and last_name and
   # returns the Author's full name.
   def full_name 
-    @full_name = "#{@first_name} #{@last_name}"
-    @full_name
+     "#{first_name} #{last_name}"
   end
 end
 
